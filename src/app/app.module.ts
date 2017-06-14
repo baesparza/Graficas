25284bc433dce1d64cec2dbd37984d13a2bdcd5c
baesparza <bruno.be81@gmail.com>
2017-06-14 17:31:40 -0500

    Linea component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { HttpModule } from '@angular/http';
 import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
+import { LineaComponent } from './components/linea/linea.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent
   ],
   imports: [
     BrowserModule,
